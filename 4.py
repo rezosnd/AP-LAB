5cd916d06d41267e248e7dec10b8bc619b290588
Rehan Suman <rehansuman41008@gmail.com>
2026-01-06 11:38:38 +0530

    question 3 and 4

---
+++ 4.py
@@ -0,0 +1,6 @@
+my_list = list(map(int, input("Enter list elements separated by space: ").split()))
+unique_list = []
+for item in my_list:
+	if item not in unique_list:
+		unique_list.append(item)
+print("List without duplicates:", unique_list)
---
+++ DAY4/3.py
@@ -0,0 +1,3 @@
+my_list = list(map(int, input("Enter list elements separated by space: ").split()))
+reversed_list = my_list[::-1]
+print("Reversed list:", reversed_list)
