d787a28477701fa46abd67eb9ab5ccdfe103e156
Rehan Suman <rehansuman41008@gmail.com>
2026-01-13 11:43:31 +0530

    day5

---
+++ DAY5/5.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 2, 3, 3, 4, 5, 5]
+
+print("Original List:", my_list)
+
+unique_list = list(set(my_list))
+
+print("After Removing Duplicates:", unique_list)
---
+++ DAY5/6.py
@@ -0,0 +1,10 @@
+# Remove Duplicates from List Using Set - Simple Version
+
+my_list = [1, 2, 2, 3, 3, 4, 5, 5]
+
+print("Original List:", my_list)
+
+# Remove duplicates using set
+unique_list = list(set(my_list))
+
+print("After Removing Duplicates:", unique_list)
